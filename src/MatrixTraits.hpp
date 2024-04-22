d22f6a66224cb455133a88ca10da97f16fa77137
Sebastiano Minelli <sebastiano.minelli@mail.polimi.it>
2024-04-22 14:03:55 +0000

    Added file MatrixTraits to store common enumerators and constexpressions

---
+++ src/MatrixTraits.hpp
@@ -0,0 +1,18 @@
+#ifndef HH_MATRIX_TRAITS_HH
+#define HH_MATRIX_TRAITS_HH
+
+#include<iostream>
+namespace algebra
+{
+    // Storage order enumerator
+enum StorageOrder
+{
+    ROW_WISE,
+    COLUMN_WISE
+};
+
+inline constexpr std::size_t DIM = 2; // Dimension of the tuple that describes a matrix element
+
+}
+
+#endif
