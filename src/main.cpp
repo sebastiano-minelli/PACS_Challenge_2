91d0579f8ebc97ea6d5e3059c7e3912ec1128d2d
Sebastiano Minelli <sebastiano.minelli@mail.polimi.it>
2024-04-22 14:05:01 +0000

    Add couts and element of class DynamicMatrix to test classes

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 
 int main()
 {
-    std::size_t n_rows; // numer of rows
-    std::size_t n_cols; // number of columns
+    std::size_t n_rows = static_cast<std::size_t>(3); // numer of rows
+    std::size_t n_cols = static_cast<std::size_t>(2); // number of columns
     std::map<std::array<std::size_t, 2>, double> elements; // elements
+
+    constexpr algebra::StorageOrder order = algebra::ROW_WISE;
 
     elements[{0, 0}] = 1.0;
     elements[{0, 1}] = 2.0;
@@ -16,6 +18,11 @@ int main()
     elements[{2, 0}] = 7.0;
     elements[{2, 1}] = 8.0;
     elements[{2, 2}] = 9.0;
+
+    algebra::DynamicMatrix<double, order> mat(n_rows, n_cols, std::move(elements));
+
+    std::cout << "columns: " << mat.columns() << std::endl;
+    std::cout << "rows: " << mat.rows() << std::endl;
 
     return 0;
 }
