93ccaf9e84dfc50a387fcf08a3faa7c3a01bc9d6
Sebastiano Minelli <sebastiano.minelli@mail.polimi.it>
2024-04-22 14:04:27 +0000

    Finished to implement DynamicMatrix class using move constructor

--- src/DynamicMatrix.hpp
+++ src/DynamicMatrix.hpp
@@ -1,43 +1,43 @@
 #ifndef HH_DYNAMIC_MATRIX_HH
 #define HH_DYNAMIC_MATRIX_HH
 
+#include "MatrixTraits.hpp"
 #include <iostream>
 #include<map>
 #include<array>
 namespace algebra
 {
-// Storage order enumerator
-enum StorageOrder
-{
-    ROW_WISE,
-    COLUMN_WISE
-};
-
-inline constexpr std::size_t DIM = 2; // Dimension of the tuple that describes a matrix element
-
-template<typename T, StorageOrder Order = StorageOrder::ROW_WISE>
+template<typename T, StorageOrder Order>
 class DynamicMatrix
 {
 
+using elements_type = std::map<std::array<std::size_t, DIM>, T>; // just to ease notation
+
 private:
-    size_t n_rows; // numer of rows
-    size_t n_cols; // number of columns
-    std::map<std::array<std::size_T, DIM>, T> elements; // elements
+    size_t n_rows = 0; // numer of rows
+    size_t n_cols = 0; // number of columns
+    std::map<std::array<std::size_t, DIM>, T> elements; // elements
 
 public:
     // leave to the compiler the default construtors
+    DynamicMatrix() = default;
+
+    // copy/move constructor
+    DynamicMatrix(const std::size_t nrows, const std::size_t ncols, elements_type && elements_)
+    : n_rows(nrows), n_cols(ncols), elements(std::forward<elements_type>(elements_)) 
+    {};
     
     // call operator (non const version)
     T& operator()(const std::size_t i, const std::size_t j)
     {
-        if (i > n_rows || j > n_cols)
-            throw std::out_of_range("Matrix access: invalid coordinates");
+        if(i > n_rows || j > n_cols)
+            throw std::out_of_range("Invalid coordinates");
 
     
         auto iter = elements.find({i, j});
 
-        if (iter == map.end())
-            return static_cast<typedef T>(0.0);
+        if(iter == elements.end())
+            return static_cast<T>(0.0);
 
         return iter->second;
     }
@@ -45,41 +45,22 @@ public:
     // call operator (const version)
     const T& operator()(const std::size_t i, const std::size_t j) const
     {
-        if (!(i < n_rows && j < n_cols))
-            throw std::out_of_range("Matrix access: invalid coordinates");
+        if(i > n_rows || j > n_cols)
+            throw std::out_of_range("Invalid coordinates");
 
-        if constexpr (Order == StorageOrder::ROW_WISE)
-        {
-            auto iter = map.find({i, j});
+        auto iter = elements.find({i, j});
         
-        if (iter == map.end())
-            return static_cast<typedef T>(0.0);
+        if(iter == elements.end())
+            return static_cast<T>(0.0);
 
         return iter->second;
     }
 
-    size_t rows() const { return n_rows; }
+    const std::size_t rows() const { return n_rows; }
 
-    size_t cols() const { return n_cols; }
-
-    template<typename U>
-    friend std::ostream& operator<<(std::ostream& os, const DynamicMatrix<U, Order>& A);
+    const std::size_t columns() const { return n_cols; }
 
 }; // end of class DynamicMatrix
-
-template<typename T, StorageOrder Order>
-std::ostream& operator<<(std::ostream& os, const DynamicMatrix<T, Order>& A)
-{
-    for (size_t i = 0; i < A.rows(); ++i)
-    {
-        for (size_t j = 0; j < A.cols(); ++j)
-            os << A(i, j) << "\t";
-
-        os << std::endl;
-    }
-
-    return os;
-}
 
 } // end of namespace algebra
 
