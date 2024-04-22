1a4287d15f09d3d70092cf0e1c17b9fcfc84b0b9
Sebastiano Minelli <sebastiano.minelli@mail.polimi.it>
2024-04-22 14:27:15 +0000

    Added some couts

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    std::size_t n_rows = static_cast<std::size_t>(3); // numer of rows
+    std::size_t n_rows = static_cast<std::size_t>(2); // numer of rows
     std::size_t n_cols = static_cast<std::size_t>(2); // number of columns
     std::map<std::array<std::size_t, 2>, double> elements; // elements
 
@@ -13,7 +13,6 @@ int main()
     elements[{0, 1}] = 2.0;
     elements[{0, 2}] = 3.0;
     elements[{1, 0}] = 4.0;
-    elements[{1, 1}] = 5.0;
     elements[{1, 2}] = 6.0;
     elements[{2, 0}] = 7.0;
     elements[{2, 1}] = 8.0;
@@ -23,6 +22,9 @@ int main()
 
     std::cout << "columns: " << mat.columns() << std::endl;
     std::cout << "rows: " << mat.rows() << std::endl;
+    std::cout << "element: " << mat(0, 2) << std::endl;
+    std::cout << "element: " << mat(1, 1) << std::endl;
+    std::cout << "element: " << mat(3, 1) << std::endl;
 
     return 0;
 }
