ac0e8c62c37a3506fd210db6b249617bd5d75969
Sebastiano Minelli <sebastiano.minelli@mail.polimi.it>
2024-04-23 15:01:41 +0000

    Added method compress

--- src/Matrix.hpp
+++ src/Matrix.hpp
@@ -7,105 +7,153 @@
 namespace algebra
 {
 template <typename T, StorageOrder Order>
-class Matrix : public DynamicMatrix<T>, public CompressedMatrix<T, Order>
+class Matrix
 {
 private:
-    std::size_t n_rows;
-    std::size_t n_cols;
-    bool is_compressed = false;
-    std::unique_pointer<MatrixBase<T>> mat;
+    bool is_compressed = false; // true = comrpessed storing
+    std::size_t n_rows = static_cast<std::size_t>(0); // number of rows
+    std::size_t n_cols = static_cast<std::size_t>(0); // number of columns
+    DynamicMatrix dynamic_mat{}; // dynamic matrix data
+    CompressedMatrix compressed_mat{}; // compressed matrix data
+
 
 public:
-    // create a matrix in uncompressed state
+    // create a matrix, set it to uncompressed state (even if it is empty)
     Matrix(const std::size_t nrows, const std::size_t ncols)
         : n_rows(nrows), n_cols(ncols)
     {
-        mat<MatrixBase<T>> = std::make_unique<DynamicMatrix<T>>(n_rows, n_cols);
+        DynamicMatrix{};
+        CompressedMatrix{};
         is_compressed = false;
+    };
+
+    // create a matrix, set it to uncompressed state
+    using elements_type = std::map<std::array<std::size_t, DIM>, T>; // just to ease notation
+    Matrix(const std::size_t nrows, const std::size_t ncols, elements_type && elements_)
+    : 
+    is_compressed(false),
+    n_rows(nrows), 
+    n_cols(ncols), 
+    dynamic_mat(std::move(elements_))
+    {
+        CompressedMatrix{};
     };
 
     void resize(const std::size_t nrows, const std::size_t ncols)
     {
-        mat->n_rows = nrows;
-        mat->n_cols = ncols;
-        
-        if(!is_compressed)
+        if(!is_compressed && (nrows < n_rows || ncols < n_cols)) // if where have a dynamic matrix we need to call the resize() method
         {
-            
+            n_rows = nrows;
+            n_cols = ncols;
+            dynamic_mat.resize(n_rows, n_cols);
+        }
+        else if(!is_compressed) // if it is dynamic but we enlarge it no need to take out elements
+        {
+            n_rows = nrows;
+            n_cols = ncols;
+        }
+        else // if(is_compressed)
+        {
+            std::cout << "Attention: the matrix will be leaved in an uncompressed state" << std::endl;
+            n_rows = nrows;
+            n_cols = ncols;
+            this->uncompress(); // uncompress takes care of adding or deleting elements
         }
     }
 
     const std::size_t rows() const { return n_rows; }
-    const std::size_t cols() const { return n_cols; }
+
+    const std::size_t columns() const { return n_cols; }
+
 
     const bool is_compressed() const
     {
-        return (Order == StorageOrder::ROW_WISE ? row_map.empty() : col_map.empty());
+        return is_compressed;
     }
 
     void compress()
     {
-        if (is_compressed())
+        if(is_compressed)
             return;
+        //else
+
+        // Clear the current compressed matrix data (just to be shure)
+        compressed_mat.clear();
+
+        compressed_mat.values.reserve(dynamic_mat.size()); // reserving space
+
+        const auto first_row = dynamic_mat.elements.cbegin()->first[0];
+        const auto last_row = dynamic_mat.elements.crbegin()->first[0];
+        const auto first_col = dynamic_mat.elements.cbegin()->first[1];
+        const auto last_col = dynamic_mat.elements.crbegin()->first[1];
 
         if constexpr (Order == StorageOrder::ROW_WISE)
         {
-            std::vector<std::pair<std::array<std::size_t, 2>, T>> vec_map(row_map.begin(), row_map.end());
-            row_map.clear();
+            // reserve space
+            compressed_mat.inner_indexes.reserve(last_row - first_row + 2); 
+            compressed_mat.outer_indexes.reserve(dynamic_mat.size());
 
-            std::sort(vec_map.begin(), vec_map.end(),
-                [](const auto& lhs, const auto& rhs)
-                {
-                    return lhs.first[0] < rhs.first[0] ||
-                        (lhs.first[0] == rhs.first[0] && lhs.first[1] < rhs.first[1]);
-                });
+            // store all column indexes inside outer_indexes
+            std::ranges::transform(dynamic_mat.elements.cbegin(),
+                                dynamic_mat.elements.cend(), 
+                                std::back_inserter(compressed_mat.outer_indexes), 
+                                [](auto& pair) { return pair->first[1]; });
 
-            row_map.emplace_back(std::make_pair(vec_map[0].first, vec_map[0].second));
-
-            for (std::size_t i = 1; i < vec_map.size(); ++i)
+            // store row indexes inside inner_indexes
+            compressed_mat.inner_indexes.push_back(0); // first element is always 0
+            
+            for(std::size_t i = first_row; i <= last_row; ++i)
             {
-                if (row_map.back().first[0] != vec_map[i].first[0])
-                {
-                    row_map.emplace_back(std::make_pair(vec_map[i].first, vec_map[i].second));
-                }
-                else
-                {
-                    row_map.back().second += vec_map[i].second;
-                }
-            }
+                auto low_bound = dynamic_mat.elements.lower_bound({i, 0});
+                auto up_bound = dynamic_mat.elements.upper_bound({i + 1, 0});
+                auto distance = std::ranges::distance(low_bound, up_bound); // computing how many elements there are in a row
+                compressed_mat.inner_indexes.push_back(distance);
+            }           
+
         }
-        else
+        else // if Order == StorageOrder::COLUMN_WISE
         {
-            std::vector<std::pair<std::array<std::size_t, 2>, T>> vec_map(col_map.begin(), col_map.end());
-            col_map.clear();
+            // reserve space
+            compressed_mat.inner_indexes.reserve(last_col - first_col + 2); 
+            compressed_mat.outer_indexes.reserve(dynamic_mat.size());
 
-            std::sort(vec_map.begin(), vec_map.end(),
-                [](const auto& lhs, const auto& rhs)
-                {
-                    return lhs.first[1] < rhs.first[1] ||
-                        (lhs.first[1] == rhs.first[1] && lhs.first[0] < rhs.first[0]);
-                });
+            // store all row indexes inside outer_indexes
+            std::ranges::transform(dynamic_mat.elements.cbegin(),
+                                dynamic_mat.elements.cend(), 
+                                std::back_inserter(compressed_mat.outer_indexes), 
+                                [](auto& pair) { return pair->first[0]; });
 
-            col_map.emplace_back(std::make_pair(vec_map[0].first, vec_map[0].second));
-
-            for (std::size_t i = 1; i < vec_map.size(); ++i)
+            // store column indexes inside inner_indexes
+            compressed_mat.inner_indexes.push_back(0); // first element is always 0
+            
+            for(std::size_t i = first_col; i <= last_col; ++i)
             {
-                if (col_map.back().first[1] != vec_map[i].first[1])
-                {
-                    col_map.emplace_back(std::make_pair(vec_map[i].first, vec_map[i].second));
-                }
-                else
-                {
-                    col_map.back().second += vec_map[i].second;
-                }
+                auto low_bound = dynamic_mat.elements.lower_bound({0, i});
+                auto up_bound = dynamic_mat.elements.upper_bound({0, i + 1});
+                auto distance = std::ranges::distance(low_bound, up_bound); // computing how many elements there are in a row
+                compressed_mat.inner_indexes.push_back(distance);
             }
+
         }
+
+        // move elements from the map to the compressed matrix
+            std::ranges::transform(dynamic_mat.elements.begin(),
+                                dynamic_mat.elements.end(), 
+                                std::back_inserter(compressed_mat.values), 
+                                [](auto& pair) { return std::move(pair->second); });        
+
+        // Change compressed state
+        is_compressed = true;
+
+        // Clear the dynamic matrix data
+        dynamic_mat.clear();
     }
+    
 
     void uncompress()
     {
         if (!is_compressed())
-            return;
+            return ;
 
         if constexpr (Order == StorageOrder::ROW_WISE)
         {
@@ -135,84 +183,20 @@ public:
 
     T& operator()(const std::size_t i, const std::size_t j)
     {
-        if (!(i < n_rows && j < n_cols))
+        if (i > n_rows || j > n_cols)
             throw std::out_of_range("Matrix index out of range");
-
-        if constexpr (Order == StorageOrder::ROW_WISE)
-        {
-            if (is_compressed())
-            {
-                auto iter = row_map.find(std::array<std::size_t, 2>{i, j});
-
-                if (iter == row_map.end())
-                {
-                    return row_map.emplace(std::make_pair(std::array<std::size_t, 2>{i, j}, T{})).first->second;
-                }
-
-                return iter->second;
-            }
-            else
-            {
-                return row_map[i * n_cols + j];
-            }
-        }
-        else
-        {
-            if (is_compressed())
-            {
-                auto iter = col_map.find(std::array<std::size_t, 2>{i, j});
-
-                if (iter == col_map.end())
-                {
-                    return col_map.emplace(std::make_pair(std::array<std::size_t, 2>{i, j}, T{})).first->second;
-                }
-
-                return iter->second;
-            }
-            else
-            {
-                return col_map[j * n_rows + i];
-            }
-        }
+        if(is_compressed)
+            return compressed_mat(i, j);
+        return dynamic_mat(i, j);
     }
 
     const T operator()(const std::size_t i, const std::size_t j) const
     {
-        if (!(i < n_rows && j < n_cols))
-            return T{};
-
-        if constexpr (Order == StorageOrder::ROW_WISE)
-        {
-            if (is_compressed())
-            {
-                auto iter = row_map.find(std::array<std::size_t, 2>{i, j});
-
-                if (iter == row_map.end())
-                    return T{};
-
-                return iter->second;
-            }
-            else
-            {
-                return row_map[i * n_cols + j];
-            }
-        }
-        else
-        {
-            if (is_compressed())
-            {
-                auto iter = col_map.find(std::array<std::size_t, 2>{i, j});
-
-                if (iter == col_map.end())
-                    return T{};
-
-                return iter->second;
-            }
-            else
-            {
-                return col_map[j * n_rows + i];
-            }
-        }
+        if (i > n_rows || j > n_cols)
+            throw std::out_of_range("Matrix index out of range");
+        if(is_compressed)
+            return const compressed_mat(i, j);
+        return const dynamic_mat(i, j);
     }
 
     template <typename U>
