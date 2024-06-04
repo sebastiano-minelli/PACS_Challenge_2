6fd998fb6dd55eb1e2fc8b602f9dbc94723a387b
Sebastiano Minelli <sebastiano.minelli@mail.polimi.it>
2024-06-04 20:30:22 +0000

    Addee openMP implementation for the for loop

--- Challenge_3/lib/InitializeProblem.hpp
+++ Challenge_3/lib/InitializeProblem.hpp
@@ -11,7 +11,7 @@ initialize_problem(const param::ParameterHandler &params, Eigen::MatrixXd &M)
 {
     const unsigned int n = params.coefficients.n; // number of intervals in the grid
     const double h = 1.0 / n; // step size
-    /*
+    
     if(n != M.rows() || n != M.cols())
     {
         std::cerr << "Error: the matrix M must have the same number of rows as the number of intervals in the grid" << std::endl;
@@ -22,25 +22,34 @@ initialize_problem(const param::ParameterHandler &params, Eigen::MatrixXd &M)
         std::cerr << "Error: the number of intervals in the grid must be at least 3" << std::endl;
         std::exit(1);
     }
-    */
+    
 
     M.setZero(); 
     std::array<double, 2> point;
+    #pragma omp parallel for shared(M)
     for(unsigned int j = 0; j < n; ++j)
     {
+        // notice that muParserXInterface isn't thread safe, we have to define a parser in every thread
+        MuParserInterface::muParserXInterface<2> BC1(params.functions.funBC_1);
+        MuParserInterface::muParserXInterface<2> BC3(params.functions.funBC_3);
+
         point = {j * h, 0.0};
-        M(n - 1, j) = params.functions.funBC_1(point); // bottom boundary
+        M(n - 1, j) = BC1(point); // bottom boundary
         
         point = {j * h, 1.0};
-        M(0, j) = params.functions.funBC_3(point); // up boundary
+        M(0, j) = BC3(point); // up boundary
     }
     for(unsigned int i = 0; i < n; ++i)
     {   
+        // notice that muParserXInterface isn't thread safe, we have to define a parser in every thread
+        MuParserInterface::muParserXInterface<2> BC2(params.functions.funBC_2);
+        MuParserInterface::muParserXInterface<2> BC4(params.functions.funBC_4);
+
         point = {0.0, i * h};
-        M(i, 0) = params.functions.funBC_4(point); // left boundary
+        M(i, 0) = BC4(point); // left boundary
 
         point = {1.0, i * h};
-        M(i, n - 1) = params.functions.funBC_2(point); // right boundary
+        M(i, n - 1) = BC2(point); // right boundary
     }
 
 }
--- Challenge_3/lib/LocalSolver.hpp
+++ Challenge_3/lib/LocalSolver.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "ParameterHandler.hpp"
 #include <Eigen/Dense>
+#include <omp.h>
 
 // here we want to solve locally a subdomain of the problem
 
@@ -44,17 +45,24 @@ class LocalSolver
             for(std::size_t k = 0; k < params.coefficients.max_it && norm_loc > params.coefficients.tol_res; ++k) // if we haven't reached the max iterations
             {
                 norm_loc = 0.0; // reset the norm
+
+                #pragma omp parallel for shared(L_loc_new) shared(L_loc) reduction(+:norm_loc)
                 // compute Jacobi iteration
                 for(std::size_t i = 1; i < n_rows - 1; ++i)
                 {
+                    // Notice that muParserXInterface isn't thread safe, we have to define a parser in every thread
+                    MuParserInterface::muParserXInterface<2> parser(params.functions.fun);
+                    
                     for(std::size_t j = 1; j < n_cols - 1; ++j)
                     {
+                        
+                        std::array<double, 2> vars = {j * h, i * h};
                         L_loc_new(i, j) = 0.25 * (
                                         L_loc(i - 1, j) + 
                                         L_loc(i + 1, j) + 
                                         L_loc(i, j - 1) + 
                                         L_loc(i, j + 1) +
-                                        h * h * params.functions.fun({j * h, i * h}) // notice that i and j are inverted
+                                        h * h * parser(vars) // notice that i and j are inverted
                                         );
                         norm_loc += (L_loc_new(i, j) - L_loc(i, j)) * (L_loc_new(i, j) - L_loc(i, j));  
                         // notice that since we aren't changing the boundary its contribution to norm_loc is zero                  
