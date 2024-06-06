ff9054d45aa9ee2bde34282d3d9ea48d68f0963c
Sebastiano Minelli <sebastiano.minelli@mail.polimi.it>
2024-06-06 21:31:23 +0000

    Changed structure to output just by rank zero

--- Challenge_3/src/main.cpp
+++ Challenge_3/src/main.cpp
@@ -8,17 +8,23 @@
 
 int main(int argc, char **argv)
 {
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     param::ParameterHandler params("data.txt");
-    params.show_data();
+    if(rank == 0)
+        params.show_data();
 
     const int N = params.coefficients.n;
 
     const double h = 1.0/ N;
 
     std::vector<double> exacSol(N * N, 0.0);
-    JacobiSolver jac_solver(exacSol, params, argc, argv);
-    MPI_Init(&argc, &argv);
-    auto [sol, norm, n_it, rank] = jac_solver.solve();
+    JacobiSolver jac_solver(exacSol, params, argc, argv, rank, size);
+    
+    auto [sol, norm, n_it] = jac_solver.solve();
     
     if(rank == 0)
     {
