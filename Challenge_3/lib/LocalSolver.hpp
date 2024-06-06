a8fff1ff54736d41308ed03add1173bb57452efd
Sebastiano Minelli <sebastiano.minelli@mail.polimi.it>
2024-06-06 17:43:20 +0000

    Added the global index for the matrix to compute the real value of the forcing term, corrected some other bugs

--- Challenge_3/lib/LocalSolver.hpp
+++ Challenge_3/lib/LocalSolver.hpp
@@ -12,20 +12,26 @@ class LocalSolver
 {
     private:
 
-        const std::size_t n; // number of rows and columns
+        std::size_t n_rows; // number of rows
+        std::size_t n_cols; // number of columns
+        const int global_row_index; // idex of the first row of the global matrix owned by the process 
         const param::ParameterHandler params; // parameters
         const std::vector<double> L; // matrix
         double norm_loc = std::numeric_limits<double>::infinity(); // local norm
 
     public:
 
-        LocalSolver(const std::vector<double> &L_, const param::ParameterHandler &params_)
-        : 
-        n(params_.coefficients.n), 
+        LocalSolver(const std::vector<double> &L_, const param::ParameterHandler &params_, const int global_row_index_)
+        :
+        global_row_index(global_row_index_),
         params(params_), 
         L(L_),
         norm_loc(std::numeric_limits<double>::infinity())
-        {};
+        {
+            // compute the number of rows and columns
+            n_rows = L.size() / params.coefficients.n;
+            n_cols = params.coefficients.n;
+        };
         
         // solves the laplace problem with Jacobi iteration locally 
         std::tuple<std::vector<double>, double> 
@@ -41,23 +47,23 @@ class LocalSolver
             // making the loop parallel
             #pragma omp parallel for shared(L_loc_new) shared(L_loc) reduction(+:norm_loc)
             // compute Jacobi iteration
-            for(std::size_t i = 1; i < n - 1; ++i)
+            for(std::size_t i = 1; i < n_rows - 1; ++i)
             {
                 // Notice that muParserXInterface isn't thread safe, we have to define a parser in every thread
                 MuParserInterface::muParserXInterface<2> parser(params.functions.fun);
                 
-                for(std::size_t j = 1; j < n - 1; ++j)
+                for(std::size_t j = 1; j < n_cols - 1; ++j)
                 {
                     
-                    std::array<double, 2> vars = {j * h, i * h};
-                    L_loc_new[i * n + j] = 0.25 * (
-                                    L_loc[(i - 1) * n + j] + 
-                                    L_loc[(i + 1) * n + j] + 
-                                    L_loc[i * n + j - 1] + 
-                                    L_loc[i * n + j + 1] +
+                    std::array<double, 2> vars = {j * h, (global_row_index + i) * h};
+                    L_loc_new[i * n_cols + j] = 0.25 * (
+                                    L_loc[(i - 1) * n_cols + j] + 
+                                    L_loc[(i + 1) * n_cols + j] + 
+                                    L_loc[i * n_cols + j - 1] + 
+                                    L_loc[i * n_cols + j + 1] +
                                     h * h * parser(vars) // notice that i and j are inverted
                                     );
-                    norm_loc += (L_loc_new[i * n + j] - L_loc[i * n + j]) * (L_loc_new[i * n + j] - L_loc[i * n + j]);  
+                    norm_loc += (L_loc_new[i * n_cols + j] - L_loc[i * n_cols + j]) * (L_loc_new[i * n_cols + j] - L_loc[i * n_cols + j]);  
                     // notice that since we aren't changing the boundary its contribution to norm_loc is zero                  
                 }
             }
